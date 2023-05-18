83a0b098058187217528c8ebe3ac8e88845c1d2b
Djouko <79266465+Djouko@users.noreply.github.com>
2023-05-18 14:55:42 +0100

    deploy data7

--- anova2_wr/settings.py
+++ anova2_wr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vux^6r@^9e!3!cuk6zbk5xlr=a6cj_eh_tn*uyv3=yu^3#1$$e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
